366cfdb796f31c5a01366121d57850e7e51866dc
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-02-29 16:11:18 +0700

    ded

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 //gsap
-gsap.set(".photo:not(:first-child)", { opacity: 0, scale: 0.5 })
+// gsap.set(".photo:not(:first-child)", { opacity: 0, scale: 0.5 })
 
-const animation = gsap.to(".photo:not(:first-child)", {
-    opacity: 1, scale: 1, duration: 1, stagger: 1
-})
+// const animation = gsap.to(".photo:not(:first-child)", {
+//     opacity: 1, scale: 1, duration: 1, stagger: 1
+// })
 
-ScrollTrigger.create({
-    trigger: ".gallery",
-    start: "top top",
-    end: "bottom bottom",
-    pin: ".petax .right",
-    animation: animation,
-    scrub: true,
-    markers: false,
-})
+// ScrollTrigger.create({
+//     trigger: ".gallery",
+//     start: "top top",
+//     end: "bottom bottom",
+//     pin: ".petax .right",
+//     animation: animation,
+//     scrub: true,
+//     markers: false,
+// })
